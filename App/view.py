259e2e81e703e4260986eba1e8d804ffdd4f3982
Maria Alejandra Estrada <m.estradag@uniandes.edu.co>
2021-09-10 10:26:49 -0500

    importar sys

--- App/view.py
+++ App/view.py
@@ -25,8 +25,10 @@ import sys
 import controller
 from DISClib.ADT import list as lt
 assert cf
+import sys
 
-
+default_limit = 1000
+sys.setrecursionlimit(default_limit*10)
 """
 La vista se encarga de la interacción con el usuario
 Presenta el menu de opciones  y  por cada seleccion
